72752e66e925fb5972cd35526d61a93b9d8cef9e
theeurbanlegend <mbuto152@gmail.com>
2025-09-10 03:33:13 +0300

    optimize

--- max_sum_of_hourglass.ts
+++ max_sum_of_hourglass.ts
@@ -2,16 +2,18 @@ function maxSum(grid: number[][]): number {
   let pt = 0;
   let max = 0;
   const m = grid.length;
-  const n = grid[0].length;
 
   while (grid[0][pt + 2] != undefined) {
     for (let i = 0; i < m; i++) {
       if (Array.isArray(grid[i + 2])) {
-        let center = grid[i + 1][pt + 1];
-        let top = grid[i][pt] + grid[i][pt + 1] + grid[i][pt + 2];
-        let bottom =
-          grid[i + 2][pt] + grid[i + 2][pt + 1] + grid[i + 2][pt + 2];
-        let sum = center + top + bottom;
+        let sum =
+          grid[i + 1][pt + 1] +
+          grid[i][pt] +
+          grid[i][pt + 1] +
+          grid[i][pt + 2] +
+          grid[i + 2][pt] +
+          grid[i + 2][pt + 1] +
+          grid[i + 2][pt + 2];
         if (max < sum) {
           max = sum;
         }
