bbd170763c8711d76dc649854ea27bc546ca9017
theeurbanlegend <mbuto152@gmail.com>
2025-10-13 16:17:16 +0300

    add function to find all unique triplets that sum to zero

---
+++ three_sum.ts
@@ -0,0 +1,35 @@
+function threeSum(nums: number[]): number[][] {
+  if (nums.length === 3) {
+    return nums.reduce((sum, arr) => sum + arr) === 0 ? [nums] : [];
+  } else {
+    nums.sort((a, b) => a - b);
+  }
+
+  const res: number[][] = [];
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+
+      if (sum === 0) {
+        res.push([nums[i], nums[left], nums[right]]);
+
+        while (left < right && nums[left] === nums[left + 1]) left++;
+        while (left < right && nums[right] === nums[right - 1]) right--;
+
+        left++;
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return res;
+}
