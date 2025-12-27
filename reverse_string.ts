3f5627ad75ff30836b3bec901864ff5caff072b4
Bewton <mbuto152@gmail.com>
2025-12-27 18:41:04 +0300

    update in-place string reversal

--- reverse_string.ts
+++ reverse_string.ts
@@ -2,5 +2,11 @@
  Do not return anything, modify s in-place instead.
  */
 function reverseString(s: string[]): void {
-  s.reverse();
-}
+    const n = s.length
+    // so i to n-1 
+    let limit = Math.floor(n / 2)
+
+    for (let i = 0; i < limit; i++) {
+        [s[i], s[n - 1 - i]] = [s[n - 1 - i], s[i]]
+    }
+};
