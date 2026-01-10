5e9b8b4b9627ff9325315fe3fd39589726f1babe
Bewton Maina <134442464+theeurbanlegend@users.noreply.github.com>
2026-01-10 12:15:22 +0300

    Simplify plusOne function implementation
    Refactor plusOne function to simplify overflow handling.

--- plus_one.ts
+++ plus_one.ts
@@ -1,19 +1,16 @@
 function plusOne(digits: number[]): number[] {
-  let m = digits.length;
-  let overflowIndex = m - 1;
-
-  digits[overflowIndex]++;
-
-  if (digits[overflowIndex] > 9) {
-    while (digits[overflowIndex] > 9) {
-      digits[overflowIndex] = 0;
-      if (digits[overflowIndex - 1] !== undefined) {
-        digits[overflowIndex - 1]++;
-      } else {
-        digits.unshift(1);
-      }
-      overflowIndex--;
+    const n = digits.length
+    for (let i = n - 1; i < n; i--) {
+        digits[i] = digits[i] + 1
+        if (digits[i] > 9) {
+            digits[i] = 0
+            if (i === 0) {
+                digits.unshift(0)
+                i = 1
+            }
+        } else {
+            break;
+        }
     }
-  }
-  return digits;
-}
+    return digits
+};
